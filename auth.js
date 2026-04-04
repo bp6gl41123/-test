7f2d61c9109414b4a707f82859b55784a8306574
bp6gl41123 <e1p3m06@gmail.com>
2026-04-05 01:22:43 +0800

    Add files via upload

--- auth.js
+++ auth.js
@@ -246,7 +246,13 @@ function handleMetaOpenUrl() {
     if (author) params.push('author=' + author);
     if (ref) params.push('ref=' + ref);
     if (params.length) url += '?' + params.join('&');
-    window.location.href = url;
+    const a = document.createElement('a');
+    a.href = url;
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
 
 
